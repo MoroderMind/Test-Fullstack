96f9a34126fa733dbdc80780109c27f0f465eb69
MoroderMind <moroderwow@gmail.com>
2025-03-11 08:32:51 +0100

    mvp with readme - used my hours, so no time for testing

--- PwtBlazor2/obj/Debug/net9.0/PwtBlazor2.AssemblyInfo.cs
+++ PwtBlazor2/obj/Debug/net9.0/PwtBlazor2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PwtBlazor2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d952906899998b805b8c0ecb80bc9506c65ff2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b7fb3bf78e849af12fa1c09dab36800300fd3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PwtBlazor2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PwtBlazor2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
